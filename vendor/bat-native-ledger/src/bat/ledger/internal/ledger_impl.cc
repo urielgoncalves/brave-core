2fcdb854e3da0a86833c65e8d9057bbecffc82b5
Jason Sadler <jsadler@brave.com>
2019-04-12 11:18:26 -0400

    Fixes brave/brave-browser#3707
    Add verified tips calculation into notification
    
    Add test for GetAmountFromVerifiedAuto
    
    Fix lint
    
    lint fixes
    
    Adding tests
    
    Combined tests to create WillTriggerNotification test
    
    spacing
    
    Updated methods to static
    
    Updated logic and test case for no publishers and zero balance
    
    lint

--- vendor/bat-native-ledger/src/bat/ledger/internal/ledger_impl.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/ledger_impl.cc
@@ -1394,8 +1394,9 @@ double LedgerImpl::GetDefaultContributionAmount() {
   return bat_state_->GetDefaultContributionAmount();
 }
 
-bool LedgerImpl::HasSufficientBalanceToReconcile() {
-  return GetBalance() >= GetContributionAmount();
+void LedgerImpl::HasSufficientBalanceToReconcile(
+    ledger::HasSufficientBalanceToReconcileCallback callback) {
+  bat_contribution_->HasSufficientBalance(callback);
 }
 
 void LedgerImpl::SaveNormalizedPublisherList(
